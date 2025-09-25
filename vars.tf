3d09deaada5cca0bac57c8adbae7548a3cb9ba26
denis.ustinov <denis.ustinov@homebuddy.com>
2025-09-25 13:23:49 +0200

    добавил дескрипшены, дефолты и переименовал переменную для бакета с историей, чтобы она отображала суть

--- vars.tf
+++ vars.tf
@@ -1,17 +1,29 @@
 variable cloud_id {
-    type = string
+  type        = string
+  default     = ""
+  description = "ID of your cloud in YC"
 }
 
 variable folder_id {
-    type = string
+  type        = string
+  default     = ""
+  description = "ID of your folder in YC"
 }
 
-variable conversation_bucket_name {
-  type    = string
-  default = "telegram-bot-conversation"
+variable conversation_bucket_prefix {
+  type        = string
+  default     = "telegram-bot-conversation"
+  description = "Prefix for conversation bucket name. to prefix add folder id"
 }
 
 variable bot_token {
-  type      = string
-  sensitive = true
+  type        = string
+  sensitive   = true
+  default     = ""
+  description = "Telegram bot token"
+}
+
+variable available_models {
+  type = list(string)
+  default = []
 }
